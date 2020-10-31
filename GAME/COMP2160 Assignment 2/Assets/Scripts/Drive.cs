91bf173a08dd39dd22d25d024e7e3287ffc7f256
44453620 <69588624+44453620@users.noreply.github.com>
2020-10-31 13:07:31 +1100

    rp

--- GAME/COMP2160 Assignment 2/Assets/Scripts/Drive.cs
+++ GAME/COMP2160 Assignment 2/Assets/Scripts/Drive.cs
@@ -6,82 +6,38 @@ public class Drive : MonoBehaviour
 {
     private float driveSpeed;
     public float rotateSpeed;
-    public float acceleration;
-    private float brake = 100;
-    private float maxSpeed = 150;
-    private float normalSpeed = 60f;
-    private float turboSpeed = 100f;
-    enum TurboState
-    {
-        Available, Active, Cooldown
-    };
-    private TurboState turboState;
-    private float turboDuration = 10;
-    public float cooldownDuration = 5;
-    private float turboTimer = 0;
+    private float acceleration;
+    private float brake;
+    private float maxSpeed = 40.0f;
+
 
     void Start()
     {
-        turboState = TurboState.Available;       
+        acceleration = 10.0f;
+        brake = 10.0f;
     }
 
     void Update()
     {
-      //driveSpeed = normalSpeed;
-      switch(turboState)
-        {
-          case TurboState.Available:
-              // driveSpeed = normalSpeed; 
-               if(Input.GetButtonDown("Turbo"))
-               {
-                   turboState = TurboState.Active;
-                   driveSpeed = turboSpeed;
-                   turboTimer = turboDuration;
-               }
-               break;
-
-          case TurboState.Active:
-               driveSpeed = turboSpeed;
-               turboTimer -= Time.deltaTime;
-               if(turboTimer <= 0)
-               {
-                   turboState = TurboState.Cooldown;
-                   driveSpeed = normalSpeed;
-                   turboTimer = cooldownDuration;
-               }
-               break;
-        
-          case TurboState.Cooldown:
-               driveSpeed = normalSpeed;
-               turboTimer -= Time.deltaTime;
-               if(turboTimer <= 0)
-               {
-                   turboState = TurboState.Available;
-               }
-               break;
-        }
-
       float forward = Input.GetAxis("Vertical");
-        if (forward > 0) {
-            driveSpeed += acceleration * Time.deltaTime;
-            }
-        else if (forward < 0) {
-            driveSpeed -= acceleration * Time.deltaTime;
-            }
-        else {
-            if(driveSpeed > 0) {
-                driveSpeed -= brake * Time.deltaTime;
-            }
+      if (forward > 0) {
+        driveSpeed += acceleration * Time.deltaTime;
+      }
+      else if (forward < 0) {
+        driveSpeed -= acceleration * Time.deltaTime;
+      }
+      else {
+        if(driveSpeed > 0) {
+            driveSpeed -= brake * Time.deltaTime;
         }
+      }
 
-      //  driveSpeed = Mathf.Clamp(driveSpeed, -maxSpeed, maxSpeed);  
+      //  driveSpeed = Mathf.Clamp(driveSpeed, -maxSpeed, maxSpeed); 
+      Vector3 velocity = Vector3.forward * driveSpeed;
+      transform.Translate(velocity * Time.deltaTime);  
 
-        transform.Translate(driveSpeed * Time.deltaTime * Vector3.forward);  
-        Vector3 velocity = Vector3.forward * driveSpeed;
-
-        float turn = Input.GetAxis("Horizontal");
-        transform.Rotate (0, turn*rotateSpeed*Time.deltaTime, 0);
-
+      float turn = Input.GetAxis("Horizontal");
+      transform.Rotate (0, turn*rotateSpeed*Time.deltaTime, 0);
     } 
 }
 
