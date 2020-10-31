93f484f6720e13493b780a79bc6f4873eaeffb71
Taro-Pattanametheekul <58500751+Taro-Pattanametheekul@users.noreply.github.com>
2020-10-31 17:24:26 +1100

    Timer
    Timer(Finished)

--- GAME/COMP2160 Assignment 2/Assets/Scripts/UIManager.cs
+++ GAME/COMP2160 Assignment 2/Assets/Scripts/UIManager.cs
@@ -8,13 +8,6 @@ public class UIManager : MonoBehaviour
 {   
     public static UIManager instance;
 
-    public Text timer;
-
-    private TimeSpan time;
-    private bool timeActive;
-
-    private float elapsed;
-
      void Awake()
     {
         if (instance != null)
@@ -31,39 +24,12 @@ public class UIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        timer.text = "Time: 00:00:00";
-        timeActive = false;
-    }
 
-    public void StartTimer()
-    {
-        timeActive = true;
-        elapsed = 0f;
-
-        StartCoroutine(UpdateTimer());
-    }
-
-    public void EndTimer()
-    {
-        timeActive = false;
-    }
-
-    private IEnumerator UpdateTimer()
-    {
-        while(timeActive)
-        {
-            elapsed += Time.deltaTime;
-            time = TimeSpan.FromSeconds(elapsed);
-            string timePlaying = "Time:" + time.ToString("mm' : 'ss' : 'ff");
-            timer.text = timePlaying;
-
-            yield return null;
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 }
