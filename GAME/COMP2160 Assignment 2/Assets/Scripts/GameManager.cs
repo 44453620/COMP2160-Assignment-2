9ad5fd9123fd02cbc8ffa2c1692a81dec0a82f15
Taro-Pattanametheekul <58500751+Taro-Pattanametheekul@users.noreply.github.com>
2020-11-02 21:41:12 +1100

    UI + Health
    - UI health
    - Smoke and Explosion effects on low health and death
    - Disabling drive on dead
    - Game Over Panel on UI

--- GAME/COMP2160 Assignment 2/Assets/Scripts/GameManager.cs
+++ GAME/COMP2160 Assignment 2/Assets/Scripts/GameManager.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject UI;
+
+    bool gameOver = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       UI.GetComponent<UIManager>().OverPanelOff();
     }
 
     // Update is called once per frame
     void Update()
     {
+       var Player = GameObject.Find("Car");
+      
+       if (Player == null)
+       {
+           gameOver = true;
+       }
        
+       if (gameOver)
+       {
+           GameOver();
+       }
+    }
+
+    void GameOver()
+    {
+        UI.GetComponent<UIManager>().OverPanelOn();
     }
 }
